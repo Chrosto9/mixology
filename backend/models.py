0635ed432b185eeee2899518d757a3e242758e25
Michał Chrostowski <mchrostowski99@gmail.com>
2023-08-17 21:29:43 +0200

    models

---
+++ backend/models.py
@@ -0,0 +1,32 @@
+from uuid import UUID, uuid4
+from enum import Enum, auto
+
+from sqlalchemy import Uuid, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class Alcohol(Enum, str):
+    VODKA = auto()
+    WHISKEY = auto()
+    RUM = auto()
+    GIN = auto()
+    TEQUILA = auto()
+    BRANDY = auto()
+    LIQUEUR = auto()
+
+class Drink(Base):
+    __tablename__ = "drinkEntity"
+
+    id: Mapped[Uuid] = mapped_column(primary_key=True, defaul=uuid4)
+    base_drink: Mapped[list[Alcohol]] = mapped_column()
+    name: Mapped[str] = mapped_column()
+    component_ids: Mapped[list[UUID]] = mapped_column(ForeignKey("componentEntity.id"))
+    drinks: Mapped["Component"] = relationship(back_populates="components")
+    
+class Component(Base):
+    __tablename__ = "componentEntity"
+
+    id: Mapped[Uuid] = mapped_column(primary_key=True, defaul=uuid4)
+    components: Mapped["Drink"] = relationship(back_populates="drinks")
